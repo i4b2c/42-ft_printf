a3232ad704160e7253bee8d78576794f281af25d
i4b2c <117470349+i4b2c@users.noreply.github.com>
2022-11-30 15:03:43 +0000

    Add files via upload

--- ft_printf.c
+++ ft_printf.c
@@ -1,5 +1,7 @@
 #include "ft_printf.h"
 #include <stdarg.h>
+#include <stdint.h>
+#include <inttypes.h>
 
 void	ft_putunbr_fd(unsigned int i, int fd)
 {
@@ -29,11 +31,52 @@ void	ft_uphexa(int n)
 	ft_putchar_fd(str[n % 16],1);
 }
 
+char *ft_strcpy(char *s, char *c)
+{
+	int i;
+	i = 0;
+	while(c[i] != '\0')
+	{
+		s[i] = c[i];
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
+}
+		
+char	ft_ptr_hexa(int n)
+{
+	char str[16] = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
+
+	return (str[n]);
+}
+
+void	ft_pointer(void * p)
+{
+	uintptr_t c;
+	char s[12];
+	int i;
+
+	i = 11;
+	c = (uintptr_t)p;
+	while((c / 16) != 0)
+	{
+		s[i] = ft_ptr_hexa(c % 16);
+		c = c / 16;
+		i--;
+	}
+	ft_putchar_fd('0',1);
+	ft_putchar_fd('x',1);
+	s[i] = ft_ptr_hexa(c);
+	ft_putstr_fd(s,1);
+}
+
 int	print_n_cont(va_list args, char ch)
 {
 	int n;
 	unsigned int un;
 	char *s;
+	void *p;
 
 	if(ch == 'c')
 	{
@@ -71,6 +114,12 @@ int	print_n_cont(va_list args, char ch)
 		ft_uphexa(n);
 		return (2);
 	}
+	else if (ch == 'p')
+	{
+		p = va_arg(args, void *);
+		ft_pointer(p);
+		return (14);
+	}
 	else
 		ft_putchar_fd('%',1);
 	return (1);
@@ -105,11 +154,13 @@ int ft_printf(char *str, ...)
 /*
 int main(void)
 {
-	char a = 126;
+	char *a = "nao deu";
 	int i;
 
-	i = ft_printf(" %s\n","boas");
-	ft_printf("quantidade de letras: %d\n",i);
+	i = ft_printf("%p\n",&a);
+	ft_printf("%d\n",i);
+	i = printf("%p\n",&a);
+	printf("%d\n",i);
 	return 0;
 
 }*/
